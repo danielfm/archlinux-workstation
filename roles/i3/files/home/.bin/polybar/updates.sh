bc0f82658eceb35c0315071202468afa8fbc83f2
Daniel Martins <daniel.tritone@gmail.com>
2020-05-08 12:01:20 -0300

    Always show the number of packages, even if it's zero

--- roles/i3/files/home/.bin/polybar/updates.sh
+++ roles/i3/files/home/.bin/polybar/updates.sh
@@ -25,13 +25,8 @@ while true; do
     # when there are updates available
     # every 10 seconds another check for updates is done
     while (( UPDATES > 0 )); do
-        if (( UPDATES == 1 )); then
-            echo " $UPDATES"
-        elif (( UPDATES > 1 )); then
-            echo " $UPDATES"
-        else
-            echo $BAR_ICON
-        fi
+        echo " $UPDATES"
+
         sleep 10
         get_total_updates
     done
@@ -39,7 +34,8 @@ while true; do
     # when no updates are available, use a longer loop, this saves on CPU
     # and network uptime, only checking once every 30 min for new updates
     while (( UPDATES == 0 )); do
-        echo $BAR_ICON
+        echo " $UPDATES"
+
         sleep 1800
         get_total_updates
     done
