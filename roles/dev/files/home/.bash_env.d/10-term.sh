c652309a6944e9134cf0def63d4b9def5625752d
Daniel Martins <daniel.tritone@gmail.com>
2020-06-01 09:35:40 -0300

    Add GOPATH var

--- roles/dev/files/home/.bash_env.d/10-term.sh
+++ roles/dev/files/home/.bash_env.d/10-term.sh
@@ -1,5 +1,7 @@
+export GOPATH=${HOME}/Projects/go
+
 # Path settings
-export PATH=${HOME}/.bin:${HOME}/.local/bin:${PATH}
+export PATH=${HOME}/.bin:${HOME}/.local/bin:${PATH}:${GOPATH}/bin
 
 # Terminal settings
 export GREP_COLOR="4;33"
