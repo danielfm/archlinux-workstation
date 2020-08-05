83112d30b1bffba7e0843a9abbe7fcd9a6205fb4
Daniel Martins <29534+danielfm@users.noreply.github.com>
2020-08-05 11:02:24 -0300

    Set up user.js script for Firefox for enhanced privacy

---
+++ roles/i3/files/home/.mozilla/firefox/user-overrides.js
@@ -0,0 +1,15 @@
+user_pref("browser.cache.disk.enable", true);
+
+user_pref("keyword.enabled", true);
+
+user_pref("privacy.clearOnShutdown.cache", true);
+user_pref("privacy.clearOnShutdown.downloads", true);
+user_pref("privacy.clearOnShutdown.formdata", true);
+user_pref("privacy.clearOnShutdown.history", true);
+user_pref("privacy.clearOnShutdown.offlineApps", true);
+user_pref("privacy.clearOnShutdown.siteSettings", true);
+
+user_pref("privacy.clearOnShutdown.cookies", false);
+user_pref("privacy.clearOnShutdown.sessions", false);
+
+user_pref("privacy.resistFingerprinting.letterboxing", false);
