b933d054688c97dbaaabb5e20ad4531fc467c2c4
Daniel Martins <29534+danielfm@users.noreply.github.com>
2020-08-07 13:28:41 -0300

    Enable websites choosing fonts

--- roles/i3/files/home/.mozilla/firefox/user-overrides.js
+++ roles/i3/files/home/.mozilla/firefox/user-overrides.js
@@ -1,9 +1,11 @@
+user_pref("browser.display.use_document_fonts", 1;
+
+user_pref("browser.cache.disk.enable", true);
+
 user_pref("browser.tabs.warnOnOpen", true);
 user_pref("browser.tabs.warnOnClose", true);
 user_pref("browser.tabs.warnOnCloseOtherTabs", true);
 user_pref("browser.tabs.warnOnQuit", true);
-
-user_pref("browser.cache.disk.enable", true);
 
 user_pref("keyword.enabled", true);
 
