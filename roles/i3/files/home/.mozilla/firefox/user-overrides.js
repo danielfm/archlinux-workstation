28de0c56090282e56e7a509feb534c52368f6e6d
Daniel Martins <29534+danielfm@users.noreply.github.com>
2020-08-20 18:48:09 -0300

    Do not override some user.js settings for increased privacy

--- roles/i3/files/home/.mozilla/firefox/user-overrides.js
+++ roles/i3/files/home/.mozilla/firefox/user-overrides.js
@@ -1,18 +1,4 @@
-user_pref("browser.cache.disk.enable", true);
-
 user_pref("browser.tabs.warnOnOpen", true);
 user_pref("browser.tabs.warnOnClose", true);
 user_pref("browser.tabs.warnOnCloseOtherTabs", true);
 user_pref("browser.tabs.warnOnQuit", true);
-
-user_pref("keyword.enabled", true);
-
-user_pref("privacy.clearOnShutdown.cache", true);
-user_pref("privacy.clearOnShutdown.downloads", true);
-user_pref("privacy.clearOnShutdown.formdata", true);
-user_pref("privacy.clearOnShutdown.history", true);
-user_pref("privacy.clearOnShutdown.offlineApps", true);
-user_pref("privacy.clearOnShutdown.siteSettings", true);
-
-user_pref("privacy.clearOnShutdown.cookies", false);
-user_pref("privacy.clearOnShutdown.sessions", false);
