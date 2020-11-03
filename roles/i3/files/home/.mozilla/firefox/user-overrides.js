f32094261298379d1320491cb09d226f845d9278
Daniel Martins <29534+danielfm@users.noreply.github.com>
2020-11-03 12:58:32 -0300

    Update user.js settings
    Signed-off-by: Daniel Martins <29534+danielfm@users.noreply.github.com>

--- roles/i3/files/home/.mozilla/firefox/user-overrides.js
+++ roles/i3/files/home/.mozilla/firefox/user-overrides.js
@@ -1,4 +1,7 @@
+user_pref("browser.display.use_document_fonts", 1);
 user_pref("browser.tabs.warnOnOpen", true);
 user_pref("browser.tabs.warnOnClose", true);
 user_pref("browser.tabs.warnOnCloseOtherTabs", true);
 user_pref("browser.tabs.warnOnQuit", true);
+
+user_pref("privacy.resistFingerprinting.letterboxing", false);
