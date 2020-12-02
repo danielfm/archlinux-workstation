b5c454fb8f43fcce8a8a66b1fdf97c780a5c7739
Daniel Martins <29534+danielfm@users.noreply.github.com>
2020-12-02 16:52:18 -0300

    Restore letterboxing for better anti-fingerprinting measures

--- roles/i3/files/home/.mozilla/firefox/user-overrides.js
+++ roles/i3/files/home/.mozilla/firefox/user-overrides.js
@@ -3,5 +3,3 @@ user_pref("browser.tabs.warnOnOpen", true);
 user_pref("browser.tabs.warnOnClose", true);
 user_pref("browser.tabs.warnOnCloseOtherTabs", true);
 user_pref("browser.tabs.warnOnQuit", true);
-
-user_pref("privacy.resistFingerprinting.letterboxing", false);
