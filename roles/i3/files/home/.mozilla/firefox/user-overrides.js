719a3a093879b677a69d6ce4e8b3e04ce8dbdeb5
Daniel Martins <29534+danielfm@users.noreply.github.com>
2020-12-20 19:44:42 -0300

    Disable autohide in fullscreen to better integrate with i3

--- roles/i3/files/home/.mozilla/firefox/user-overrides.js
+++ roles/i3/files/home/.mozilla/firefox/user-overrides.js
@@ -1,4 +1,5 @@
 user_pref("browser.display.use_document_fonts", 1);
+user_pref("browser.fullscreen.autohide", false);
 user_pref("browser.tabs.warnOnOpen", true);
 user_pref("browser.tabs.warnOnClose", true);
 user_pref("browser.tabs.warnOnCloseOtherTabs", true);
